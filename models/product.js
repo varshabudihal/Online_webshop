da380ed59c6b1d8adda6f9be95079435f0a1258b
Varsha Budihal <varsha.budihal@gmail.com>
2019-11-03 13:30:45 -0800

    push to heroku

--- models/product.js
+++ models/product.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
@@ -10,16 +9,3 @@ price:{type: Number, required: true}
 });
 
 module.exports = mongoose.model('Product', schema);
-=======
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var schema = new Schema({
-imagePath:{type: String, required: true},
-title:{type: String, required: true},
-description:{type: String, required: true},
-price:{type: Number, required: true}
-});
-
-module.exports = mongoose.model('Product', schema);
->>>>>>> 96a6cf65ab3db2402de34167be03ad6b892017f1
